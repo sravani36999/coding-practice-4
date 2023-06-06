d75208b05e01ac85a832be5cefd84a6db6e39186
sravanim <sravanim1505@gmail.com>
2023-06-06 11:43:32 +0000

    all test cases are failed

--- app.js
+++ app.js
@@ -48,3 +48,73 @@ app.get("/players/", async (request, response) => {
     )
   );
 });
+
+//created a new player in the team(database). player_id is aut-incremented
+
+app.post("/players/", async(request, response) => {
+    const playerDetails = request.body;
+    const {playerName, jerseyNumber, role} = playerDetails;
+    const addPlayerQuery = 
+    `INSERT INTO 
+    cricket_team (player_name, jersey_number, role) 
+    VALUES (
+    `${playerName}`,
+    `${jerseyNumber}`,
+    `${role}`);`;
+
+    const dbResponse = await db.run(addPlayerQuery);
+    response.send("Player Added to Team");
+})
+
+//Returns a player based on player ID
+app.get("/players/:playerId/" , async(request, response) => {
+    const {playerId} = request.params;
+    const getPlayerQuery = `
+    SELECT * FROM 
+    cricket_team 
+    WHERE
+    player_id = `${playerId}`;`;
+
+    const player = await db.get(getPlayerQuery);
+
+    response.send(convertDbObjectToResponseObject(player));
+});
+
+//Updates the details of a player in the team (database) based on the player ID
+
+app.put("players/:playerId/" ,async (request, response) => {
+    const {playerId} = request.params;
+    const playerDetails = request.body;
+    const {playerName,jerseyNumber, role} = playerDetails;
+    const updatedQuery = `UPDATE 
+    cricket_team 
+    SET 
+    player_name = `${playerName}`,
+    jersey_number = `${jerseyNumber}`,
+    role = `${role}`
+    WHERE 
+    player_id = ${playerId};`;
+
+    await db.run(updatedQuery);
+    response.send("Player Details Updated");
+});
+
+app.delete("/players/:playerId/", async(request, response) => {
+    const playerId = request.params;
+    const deletePlayerQuery = `
+    DELETE
+    FROM 
+    cricket_team 
+    WHERE 
+    player_id = `${playerId}`;`;
+
+    await db.run(deletePlayerQuery);
+
+    response.send("Player Removed");
+
+});
+
+ module.exports = app;
+
+
+
